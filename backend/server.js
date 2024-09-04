5a450fd7c133e9d88f22176210647a567574b3a1
Rufai T <52770296+Rufi-j@users.noreply.github.com>
2024-09-04 15:55:52 +0100

    reversal on serjs

--- backend/server.js
+++ backend/server.js
@@ -30,29 +30,29 @@ app.use('/api/user', userRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
-    // .then(() => {
-    //     // listen for requests
-    //     app.listen(process.env.PORT, () => {
-    //         console.log('connected to db & listening on port', process.env.PORT);
-    //     });
-    // })
-    // .catch((error) => {
-    //     console.log(error);
-    // }); 
     .then(() => {
-        console.log('Connected to the database');
-
-        // Check if running locally and start listening
-        if (process.env.NODE_ENV !== 'production') {
-            const port = process.env.PORT || 4000;
-            app.listen(port, () => {
-                console.log('Server running on port', port);
-            });
-        }
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.error('Database connection error:', error);
-    });
+        console.log(error);
+    }); 
+//     .then(() => {
+//         console.log('Connected to the database');
+
+//         // Check if running locally and start listening
+//         if (process.env.NODE_ENV !== 'production') {
+//             const port = process.env.PORT || 4000;
+//             app.listen(port, () => {
+//                 console.log('Server running on port', port);
+//             });
+//         }
+//     })
+//     .catch((error) => {
+//         console.error('Database connection error:', error);
+//     });
 
 
-module.exports = app;  //new
+// module.exports = app;  //new
