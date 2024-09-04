c9eead9b67564559b68a0c7adcba41bdb49880d2
Rufai T <52770296+Rufi-j@users.noreply.github.com>
2024-09-04 16:27:38 +0100

    final answer

--- backend/server.js
+++ backend/server.js
@@ -29,41 +29,31 @@ app.use('/api/workouts/', workoutRoutes);
 app.use('/api/user', userRoutes);
 
 // connect to db
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => {
-//         // listen for requests
-//         app.listen(process.env.PORT, () => {
-//             console.log('connected to db & listening on port', process.env.PORT);
-//         });
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     }); 
-
-    mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
+    // .then(() => {
+    //     // listen for requests
+    //     app.listen(process.env.PORT, () => {
+    //         console.log('connected to db & listening on port', process.env.PORT);
+    //     });
+    // })
+    // .catch((error) => {
+    //     console.log(error);
+    // }); 
+    
     .then(() => {
         console.log('Connected to the database');
+
+        // Check if running locally and start listening
+        if (process.env.NODE_ENV !== 'production') {
+            const port = process.env.PORT || 4000;
+            app.listen(port, () => {
+                console.log('Server running on port', port);
+            });
+        }
     })
     .catch((error) => {
         console.error('Database connection error:', error);
     });
 
-// Export the app for serverless deployment
-module.exports = app;
-//     .then(() => {
-//         console.log('Connected to the database');
 
-//         // Check if running locally and start listening
-//         if (process.env.NODE_ENV !== 'production') {
-//             const port = process.env.PORT || 4000;
-//             app.listen(port, () => {
-//                 console.log('Server running on port', port);
-//             });
-//         }
-//     })
-//     .catch((error) => {
-//         console.error('Database connection error:', error);
-//     });
-
-
-// module.exports = app;  //new
+module.exports = app;  //new
